853d941ca8dfe67144a53f8d3631d99814012e37
TSK <2939117014@qq.com>
2021-07-23 17:25:14 +0800

    小修

--- src/components/common/Aside/index.vue
+++ src/components/common/Aside/index.vue
@@ -1,7 +1,7 @@
 
 <template>
   <div style="height: 100vh">
-    <el-aside :width="componentWidth" style="background: #304156;height:100vh">
+    <el-aside style="background: #304156; height: 100vh" width="auto">
       <el-menu
         router
         :default-active="$route.path"
@@ -11,20 +11,21 @@
         overflow-y
         scroll
         active-text-color="#409eff"
+        class="el-menu-vertical-demo"
       >
         <el-header class="header">
-          <div class="text-truncate text-center text-white">
+          <div class="text-center text-white">
             <span style="fontsize: 14px">后台管理系统</span>
           </div>
         </el-header>
-        <div v-for="(item, key) in routes" :key="item.path">
+
+        <div v-for="(item, key) in router" :key="item.path">
           <el-submenu :index="key + ''">
             <template #title>
+              <i :class="['iconfont', item.meta?.icon]"></i>
 
-              <i :class="['iconfont',item.meta?.icon]"></i>
-
-              <span :class="[{ 'd-none': isCollapse },'ms-2']">
-                {{ item.name || "" }}</span
+              <span :class="[{ 'd-none': isCollapse }, 'ms-2']">
+                {{ item.meta?.title || "" }}</span
               >
             </template>
             <el-menu-item-group
@@ -32,7 +33,7 @@
               :key="childKey"
             >
               <el-menu-item :index="child.path">
-                {{ child.name }}
+                {{ child.meta?.title }}
               </el-menu-item>
             </el-menu-item-group>
           </el-submenu>
@@ -42,30 +43,38 @@
   </div>
 </template>
 
-<script>
-import { routes } from "../../../router";
-export default {
-  computed: {
-    routes() {
-      return routes || [];
-    },
-    hasChildren() {
-      return function (item) {
+<script lang="ts">
+import { State } from "../../../store/index";
+import { useStore } from "vuex";
+import { routes, routeItem } from "../../../router";
+import { defineComponent, computed, ref } from "vue";
+export default defineComponent({
+  setup() {
+    const store = useStore<State>();
+ 
+    const isCollapse = computed(() => {
+      return store.state.isCollapse;
+    });
+    const hasChildren = computed(() => {
+      return function (item: routeItem) {
         return item.children || [];
       };
-    },
-    isCollapse() {
-      return this.$store.state.isCollapse;
-    },
-    componentWidth(){
-      if(this.isCollapse){
-        return "fit-content"
-      }else {
-        return "200px"
-      }
-    }
+    });
+
+    const router = computed<routeItem[]>(() => {
+      return routes.filter((item) => !item.meta.isNotSideBar);
+    });
+
+
+
+    return {
+      isCollapse,
+      hasChildren,
+      router,
+     
+    };
   },
-};
+});
 </script>
 
 <style scoped lang="scss">
@@ -80,6 +89,7 @@ export default {
   font-weight: 600;
   font-size: 14px;
   vertical-align: middle;
+  overflow: hidden;
 }
 ::v-deep(.el-menu) {
   border: none;
@@ -87,7 +97,11 @@ export default {
 ::v-deep(.el-menu-item) {
   background-color: #1f2d3d !important;
 }
-::v-deep(.el-menu-item-group__title){
+::v-deep(.el-menu-item-group__title) {
   padding: 0 !important;
 }
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
 </style>
