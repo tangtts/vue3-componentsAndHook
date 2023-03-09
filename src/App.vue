650d956dc7cbcc51aedc7ca04c6cafc677ff4fda
tangtts <2939117014@qq.com>
2023-03-09 17:28:33 +0800

    style:优化全局组件

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,8 @@
       <!-- <sidePicPreview /> -->
       <!-- <myUpload /> -->
       <!-- <myTree /> -->
-      <!-- <myComponent as="button" type="submit">点击</myComponent> -->
       <!-- <virtualList /> -->
       <!-- <BaGua animationSpeed='1s' size="100"/> -->
-      <!-- <div v-bagua-loading="{size:100,animationSpeed:'1s'}">
-        1232131
-      </div> -->
 
       <!-- <preview-img img="../docs/public/logo.png" v-model="show" /> -->
       <!-- <el-button @click="show =!show" type="primary"> 打开 </el-button> -->
@@ -28,52 +24,13 @@
   </div>
 </template>
 <script lang="ts" setup>
-// import myComponent from "./components/button/index"
-// import virtualList from "./components/virtual/index.vue"
-// import BaGua from './components/baGua/index.vue'
 // import BaGua from './components/baGua/BaGua.vue'
-import InfiniteScroll from './components/InfiniteScroll/index.vue'
-// import previewImg from "./components/previewImage/index.vue"
-import magnifiers from "./components/magnifiers/index.vue"
-import reverseCard from "./components/reverse-card/index.vue"
 import Loading from "./components/loading/index.vue"
 import clickOutside from "./components/clickOutside/index.vue"
-import JinCai from "./components/jianCaiUpload/index.vue"
-import myTree from "./components/tree/index.vue"
-import AddKettleDynamically from "./components/AddKettleDynamically/index.vue"
-import sidePicPreview from "./components/sidePicPreview/index.vue"
 import { createApp, defineComponent, onMounted, h, ref, createVNode, render } from "vue";
 import { ObjectDirective, FunctionDirective } from "vue"
 import zhCn from "element-plus/lib/locale/lang/zh-cn";
 const locale = ref(zhCn);
-const show = ref(false)
-
-
-
-// const instance = loadingInstance.mount(document.createElement('div'))
-
-
-
-// const vBaguaLoading = {
-//   mounted: (el, bindings, vnode) => {
-//     const loadingInstance = createVNode(BaGua)
-//     let containter = document.createElement('div')
-//     render(loadingInstance, containter)
-//     // el 是当前绑定的元素
-//     // instance 是引入的虚拟 dom，只有 $el
-//     // 需要把 el.appendChild(instance.$el)
-//     if (bindings.value) {
-//       let f = loadingInstance.component?.props
-//       f!.size = bindings.value.size
-//       f!.animationSpeed = bindings.value.animationSpeed
-//       //  instance.$props = bindings.value
-//     }
-
-//     // el.appendChild(instance.$el)
-//     el.appendChild(containter)
-//   },
-// }
-
 
 </script>
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,24 +1,49 @@
+
+import { App, Component } from "vue";
 import { checkboxPropsTypes } from "./checkbox/types";
 import uploadPage from "components/upload/index.vue";
-import TreePage from "components/tree/index.vue";
+import treePage from "components/tree/index.vue";
 import myCheckbox from "components/checkbox/index.vue";
-import DatePickerPage from "components/date-picker/index.vue";
+import datePickerPage from "components/date-picker/index.vue";
 import { UploadProps } from "./upload/types";
+import sidePicPreview from "components/sidePicPreview/index.vue"
+import addKettleDynamically from "components/addKettleDynamically/index.vue"
+import myTree from "components/tree/index.vue"
+import JinCai from "components/jianCaiUpload/index.vue"
+import virtualList from "components/virtual/index.vue"
+import infiniteScroll from 'components/infiniteScroll/index.vue'
+import magnifiers from "components/magnifiers/index.vue"
+import reverseCard from "components/reverse-card/index.vue"
+import previewImg from "components/previewImage/index.vue"
 
-import { App } from "vue";
 
 declare module "*.vue" {
   export interface GlobalComponents {
     uploadPage: UploadProps;
     myCheckbox: checkboxPropsTypes;
+    datePickerPage:any
   }
 }
 
-const Components = [uploadPage, TreePage, myCheckbox, DatePickerPage];
+const Components: Array<{ name: string; component: Component }> = [
+  { name: "uploadPage", component: uploadPage },
+  { name: "treePage", component: treePage },
+  { name: "myCheckbox", component: myCheckbox },
+  { name: "datePickerPage", component: datePickerPage },
+  { name: "sidePicPreview", component: sidePicPreview },
+  { name: "addKettleDynamically", component: addKettleDynamically },
+  { name: "myTree", component: myTree },
+  { name: "JinCai", component: JinCai },
+  { name: "virtualList", component: virtualList },
+  { name: "infiniteScroll", component: infiniteScroll },
+  { name: "magnifiers", component: magnifiers },
+  { name: "reverseCard", component: reverseCard },
+  { name: "previewImg", component: previewImg },
+];
 
 function install(app: App) {
   Components.forEach(component => {
-    app.component(component.name, component);
+    app.component(component.name, component.component);
   });
 }
 export default install;
